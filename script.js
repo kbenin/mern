c9212f4f7ffc71f97cbf6017270957833f1b1a55
kbenin <112169415+kbenin@users.noreply.github.com>
2025-08-17 16:03:50 +0800

    initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+console.log('Test!');
+
+var x = 10;
+var y = 20; 
+
+var z = x + y;
+console.log('The sum of x and y is:', z);
+
+
+function greet(name) {
+    console.log('Hello, ' + name + '!');
+}
+
+greet('Alice');
+
+5 === '5'    
+
+function greeting(name) {
+    console.log('Hello, ' + name + '!');
+}
+
+greeting('Kenn');
